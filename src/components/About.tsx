96012ad399f1f69517c72a35163cf9adb9c5501d
MRSevim <lionrasit@gmail.com>
2025-09-18 23:05:47 +0300

    updated about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,10 +5,10 @@ const About = () => {
     <div className="mt-15">
       <h1 className="text-2xl font-bold ">About</h1>
       <p className="mt-2">
-        This website is done as personal tool. I needed something to track my
-        day to day mood so I developed something simple like this. However,
-        anyone can create an account and start tracking their mood. So feel free
-        to do so and and let me know what you think!
+        This website is a nice tool for users to track their day to day mood and
+        see how it changes over time. I needed something to track my mood so I
+        developed it. However, anyone can create an account and start tracking
+        their mood. So feel free to do so and and let me know what you think!
       </p>
       <h2 className="mt-2 text-xl font-bold">More About Me</h2>
       <p className="mt-2">
@@ -18,7 +18,7 @@ const About = () => {
         fully functional web app but is just a fun little project that some
         people might find useful. However, I am open to suggestions on how to
         make it nicer and might implement more stuff to the thing in the future.
-        If you are curious about other things I built check my portfolio{" "}
+        If you are curious about other things I built, check my portfolio{" "}
         <Link
           href={"https://mrsevim.github.io/Portfolio/"}
           className="hover:underline"
